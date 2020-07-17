3bb26181022ac03b6c0894bccd7c4f86d50d6c41
thrucvoid19 <thrucovid19@gmail.com>
2020-07-17 15:28:44 -0500

    Added filter for aws_ami to select ebs backed ubuntu instance

--- web-server/main.tf
+++ web-server/main.tf
@@ -7,6 +7,11 @@ provider "aws" {
 data "aws_ami" "ubuntu-linux" {
   most_recent = true
 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
   owners = ["099720109477"]
 }
 
