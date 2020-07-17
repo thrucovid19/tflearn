8d0d0373bb2e770c37d2e10c7d2ea612bdbb6407
thrucvoid19 <thrucovid19@gmail.com>
2020-07-17 15:00:24 -0500

    Add EC2 web server

---
+++ web-server/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  version                 = "~> 2.0"
+  shared_credentials_file = var.credentials
+  region                  = var.region
+}
+
+data "aws_ami" "ubuntu-linux" {
+  most_recent = true
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "tf-web" {
+  ami                    = data.aws_ami.ubuntu-linux.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.tf-http-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  tags = {
+    Name = "tf-web-server"
+  }
+}
+
+# Create security group
+
+resource "aws_security_group" "tf-http-sg" {
+  name = "tf-http-sg"
+
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ web-server/variables.tf
@@ -0,0 +1,14 @@
+variable "credentials" {
+  default = "~/.aws/credentials"
+}
+
+variable "region" {
+  description = "EC2 Region for the VPC"
+  default     = "us-east-1"
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
