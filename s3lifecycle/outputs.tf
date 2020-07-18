c89f3c0ad2e2b79762d0b73483686009ea5c59c4
thrucvoid19 <thrucovid19@gmail.com>
2020-07-17 22:32:18 -0500

    Modified s3lifecycle to include bucket policy as well

--- s3lifecycle/outputs.tf
+++ s3lifecycle/outputs.tf
@@ -1,4 +1,4 @@
 output "bucket-arn" {
-  value       = aws_s3_bucket.tf-lifcycle-bucket.arn
+  value       = aws_s3_bucket.tf-lifecycle-bucket.arn
   description = "Arn of the new bucket with lifecycle policy"
 }
