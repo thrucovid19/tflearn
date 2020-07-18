ab056de69fb0cadf80f9d82be3ec82df50a5909f
thrucvoid19 <thrucovid19@gmail.com>
2020-07-17 23:06:58 -0500

    Added a python lambda function

---
+++ lambda/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">=0.12"
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "all-projects-tf-states-727836929659"
+    key    = "lambda/terraform.tfstate"
+    region = "us-east-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "all-project-tf-locks-727836929659"
+    encrypt        = true
+  }
+}
